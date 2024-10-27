c3582a0a58849e06d561799a433049d96f2d6930
Showrab15 <supto50showrab50showrab@gmail.com>
2024-10-27 14:27:17 +0600

    users admin updated

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors');
+// require('dotenv').config()
+
+// const port = process.env.PORT || 5000
+// //middleware
+// app.use(cors());
+// app.use(express.json());
+
 const express = require('express');
-const app = express();
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
-const port = process.env.PORT || 5000
-//middleware
-app.use(cors());
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Specify allowed methods
+    credentials: true // Allow credentials if needed
+}));
 app.use(express.json());
-
-
 
 const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bter72s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
